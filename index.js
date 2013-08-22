4498aac9749fd26ad0552ee41f137ee020f032fb
William PETIT <william.petit@ac-dijon.fr>
2013-08-22 10:55:44 +0200

    Fix typo

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = exports = {
   App: require('./lib/app'),
-  DependencyGraph: require('./lib/dependecy-graph'),
+  DependencyGraph: require('./lib/dependency-graph'),
   Error: require('./lib/error')
 };
